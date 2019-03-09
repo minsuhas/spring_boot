5b43d6a8cfd53f560e212c96945ab0cce0ff6c1f
minsu <karosis28@gmail.com>
2019-03-10 00:27:28 +0900

    mission 1 /hello 주소로 접금 시 아래의 HTML을 반환 하는 기능 추가

---
+++ src/main/java/com/minsuhas/springboot/controller/MinHasController.java
@@ -0,0 +1,15 @@
+package com.minsuhas.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MinHasController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "hello";
+    }
+
+}
